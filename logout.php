e70ff3a5bb9733ecd1abc8bdd13715255f4fb05f
gsiqueiros <51829770+gsiqueiros@users.noreply.github.com>
2019-06-14 08:47:07 -0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,8 @@
 <?php
 //este codigo es para iniciar sesion :D
-    session_start();
+  
+//segunda modificacion
+session_start();
     $_SESSION = array();
     session_destroy();
          echo '<script>window.location.href="login.php";</script>';
