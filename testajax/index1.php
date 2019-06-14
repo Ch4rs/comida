b1230b542d059b154a9b443ef046d3114974bad7
NoanHC <noanhc77@gmail.com>
2019-06-14 09:04:30 -0700

    testajax
    test

---
+++ testajax/index1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prueba ajax</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="main.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link type="text/css" rel="stylesheet" href="css/estilos.css"/>
+</head>
+<body>
+	<div>
+		<center>
+		<h1>jQuery envio de datos ajax</h1><br>
+		<form>
+			<label for="username">Usuario</label><br>
+			<input class="form-control" type="text" name="username" id="username"><br>
+			<label for="password">Password</label><br>
+			<input class="form-control" type="text" name="password" id="password"><br>
+			
+			<input class="btn btn-primary" type="submit" name="" value="login">
+			<i class="fas fa-sync fa-spin"></i>
+		</form>
+		<span></span>
+	</center>
+	</div>
+
+</body>
+</html>
---
+++ testajax/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+	$('form').submit(function(e){
+		e.preventDefault();
+
+		var data = $(this).serializeArray();
+		data.push({name: 'tag', value: 'login'});
+		$.ajax({
+			url: 'process.php',
+			type: 'post',
+			dataType: 'json',
+			data: data,
+
+			beforeSend:function(){
+				$('.fa').css('display','inline');
+			}
+		})
+		.done(function(){ //true
+			$('span').html("Correcto");
+		})
+		.fail(function(){ //false
+			$('span').html("Falso");
+		})
+		.always(function(){
+			setTimeout(function(){
+			$('.fas').hide();	
+		}, 1000);
+			
+
+		});
+	})
+})
